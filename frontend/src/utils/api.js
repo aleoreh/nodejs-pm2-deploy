d9f065f003d4713b967e09a1b16bb2226cdf83fe
Aleksey Orekhov <aleoreh@gmail.com>
2024-12-22 15:48:57 +0300

    frontend api fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('api.aleoreh.students.nomorepartiesco.ru');
+  const api = new Api('https://api.aleoreh.students.nomorepartiesco.ru');
 
   export default api;
