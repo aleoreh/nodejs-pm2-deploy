492b78d1c53507640c45e130fd007285603878d9
Aleksey Orekhov <aleoreh@gmail.com>
2024-12-22 14:59:57 +0300

    pm2 config for backend

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,32 @@
+require('dotenv').config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'app',
+      script: './dist/app.js',
+      watch: '.',
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/aleoreh/nodejs-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp -Cr .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source`,
+      'post-deploy':
+        'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'pre-setup': '',
+    },
+  },
+};
